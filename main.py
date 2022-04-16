465da012429d3c5bda3ad57997e20e0403f69764
Charles Livuza <livuzacharles@gmail.com>
2022-04-16 06:01:04 +0300

    underweight, normal weight, overweight, obese, clinically obese

--- main.py
+++ main.py
@@ -10,3 +10,26 @@ bmi_weight=float(weight)
 raw_bmi=bmi_weight/(bmi_height**2)
 bmi=round(raw_bmi)
 print(bmi)
+
+# DON'T CHANGE HE CODE BELOW
+height = input("Enter your height in m: ")
+weight = input("Enter your weight in kg: ")
+# DON'T CHANGE THE CODE ABOVE
+
+# ----------------------------------------- #
+# WRITE YOUR CODE BELOW
+bmi_height = float(height)
+bmi_weight = float(weight)
+raw_bmi = bmi_weight/(bmi_height**2)
+bmi=round(raw_bmi)
+if bmi < 18.5:
+    status = "underweight"
+if bmi <= 25:
+    status = "normal weight"
+elif bmi <=30:
+    status = "slightly overweight"
+elif bmi <=35:
+    status = "obese"
+else:
+    status = "clinically obese"
+print(f"Your BMI is {bmi}. Your status is {status}")
